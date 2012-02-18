f2cc7ee10cf32eba82efc130231f72c7cca69e35
fancyoung <siyang1982@msn.com>
2012-02-18 18:35:55 +0800

    -

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -8,7 +9,6 @@ class User
          :recoverable, :rememberable, :trackable, :validatable, :encryptable
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  validates_uniqueness_of :name, :email, :case_sensitive => false
   
   ## Database authenticatable
   field :name,              :type => String, :null => false, :default => ""
@@ -45,4 +45,27 @@ class User
 
   ## Token authenticatable
   # field :authentication_token, :type => String
+
+  validates_presence_of :name,
+    message: "请输入用户名"
+  validates_format_of :name, 
+    with: /^[0-9a-zA-Z\u4e00-\u9fa5]+$/i, 
+    message: "名字中不能包含特殊字符"
+  validates_length_of :name,
+    within: 1..40,
+    too_short: "最少%{count}个字",
+    too_long: "最多%{count}个字"
+  validates_uniqueness_of :name, 
+    case_sensitive: false, 
+    message: "此用户名已被使用"
+  validates_length_of :name,
+    within: 1..40,
+    too_short: "最少%{count}个字",
+    too_long: "最多%{count}个字"
+
+  before_create :no_password_confirmation
+
+  def no_password_confirmation
+    self.password_confirmation = self.password
+  end
 end
