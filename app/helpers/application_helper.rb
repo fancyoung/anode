d74ed4dcdcd9265bc47542dbec6e44046020477b
fancyoung <siyang1982@msn.com>
2012-02-20 18:57:40 +0800

    add default avatar. add post css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def is_active?(page_name)
     "active" if params[:controller] == page_name
   end
+
+  DEFAULT_AVATAR = {
+    :large => "/assets/avatar_large.jpg",
+    :medium => "/assets/avatar_medium.jpg",
+    :small => "/assets/avatar_small.jpg",
+  }
+  def get_avatar(user, size)
+    if user && user.avatar && user.avatar.url
+      "#{user.avatar.url}-#{size}"
+    else
+      DEFAULT_AVATAR[size.to_sym]
+    end
+  end
 end
