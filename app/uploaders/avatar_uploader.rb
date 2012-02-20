e6501d4a68eacf761407290c337508258a66c444
huang <siyang1982@msn.com>
2012-02-20 21:32:44 +0800

    use unique image name

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,3 +1,10 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   storage :upyun
+
+  def filename
+    if original_filename 
+      @name ||= Digest::MD5.hexdigest(File.dirname(current_path))
+      "#{@name}.#{file.extension}"
+    end
+  end
 end
